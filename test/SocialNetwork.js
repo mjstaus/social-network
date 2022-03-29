8d966e9114ffe0260408d3627daeb84fcfc6f54e
Martha Staus <mjstaus@gmail.com>
2022-03-29 12:49:43 -0700

    TEST: Add test for creating post

--- test/SocialNetwork.js
+++ test/SocialNetwork.js
@@ -7,7 +7,7 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-  contract('SocialNetwork', (accounts) => {
+  contract('SocialNetwork', ([deployer, author, tipper]) => {
     let socialNetwork
 
     before(async () => {
@@ -24,19 +24,32 @@ require('chai')
       })
       it('has a name', async() => {
         const name = await socialNetwork.name();
-        assert.deepEqual(name, "Dapp University Social Network")
+        assert.deepEqual(name, 'Dapp University Social Network')
       })
     })
     
     describe('posts', async () => {
+      let result, postCount
       it('creates posts', async() => {
+        result = await socialNetwork.createPost('hello!', { from: author });
+        postCount = await socialNetwork.postCount();
         
-      })
-      it('lists all the posts', async() => {
+        // SUCCESS
+        assert.equal(postCount, 1);
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), postCount.toNumber(), 'id is correct');
+        assert.equal(event.content, 'hello!', 'content is correct');
+        assert.equal(event.tipAmount, '0', 'tipAmount is correct');
+        assert.equal(event.author, author, 'author is correct');
 
+        // FAILURE: Post must have content
+        await socialNetwork.createPost('', { from: author }).should.be.rejected;
       })
-      it('allows users to tip posts', async() => {
+      // it('lists all the posts', async() => {
 
-      })
+      // })
+      // it('allows users to tip posts', async() => {
+
+      // })
     })
   })
