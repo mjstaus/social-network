dc8ee1a097791838fe6c6e2cdaca37a672165f11
Martha Staus <mjstaus@gmail.com>
2022-04-02 13:01:49 -0700

    REFACTOR: Move navbar code  into Navbar.js component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Web3 from "web3";
 import logo from "../logo.png";
 import "./App.css";
 
+import Navbar from "./Navbar";
+
 class App extends Component {
 
   constructor(props) {
@@ -41,23 +43,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-secondary">
-                <small id="account">{this.state.account}</small>
-              </small>
-            </li>
-          </ul>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
